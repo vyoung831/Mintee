85478377be5ff214ede4ffe0a9512d24119ec83d
Vincent Young <v.young831@yahoo.com>
2020-04-08 07:46:12 -0400

    Set up SwiftUI and set MainTabBarView with basic tab bar as new landing page

---
+++ Mu/swift-ui/TodayView.swift
@@ -0,0 +1,21 @@
+//
+//  TodayView.swift
+//  Mu
+//
+//  Created by Vincent Young on 4/8/20.
+//  Copyright © 2020 Vincent Young. All rights reserved.
+//
+
+import SwiftUI
+
+struct TodayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TodayView_Previews: PreviewProvider {
+    static var previews: some View {
+        TodayView()
+    }
+}
