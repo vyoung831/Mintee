d39b5b328d39533f516786d70c4d3c8d3b250ba9
Vincent Young <v.young831@yahoo.com>
2020-06-20 18:56:40 -0400

    Added unit tests for Date extensions

--- MuTests/BubbleRowsTests.swift
+++ MuTests/BubbleRowsTests.swift
@@ -1,5 +1,5 @@
 //
-//  BubbleRowsTestCase.swift
+//  BubbleRowsTests.swift
 //  MuTests
 //
 //  Created by Vincent Young on 6/16/20.
@@ -9,7 +9,7 @@
 import XCTest
 @testable import Mu
 
-class BubbleRowsTestCase: XCTestCase {
+class BubbleRowsTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
