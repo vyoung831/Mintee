1cc7e241c6327243e498e17a3d7d44d6f7bbe65c
Vincent Young <v.young831@yahoo.com>
2020-06-29 06:37:01 -0400

    Added unit test for Task's deleteSelf function

--- MuTests/core-data/task-tests/TaskTests.swift
+++ MuTests/core-data/task-tests/TaskTests.swift
@@ -80,6 +80,63 @@ class TaskTests: XCTestCase {
     }
     
     /**
+     Test that updateDates updates the Task's dates and saves in the expected String format
+     */
+    func testUpdateDates() {
+        
+        let task = Task(context: CDCoordinator.moc)
+        XCTAssert(task.startDate == nil)
+        XCTAssert(task.endDate == nil)
+        
+        // Start date = GMT April 17, 1998 13:49:35
+        // End date = GMT December 5, 1998 01:22:55
+        let startDate = Date(timeIntervalSince1970: 892820975)
+        let endDate = Date(timeIntervalSince1970: 912820975)
+        let startDay = String(format: "%02d", Calendar.current.component(.day, from: startDate))
+        let endDay = String(format: "%02d", Calendar.current.component(.day, from: endDate))
+        let startDateString = "1998-04-\(startDay)"
+        let endDateString = "1998-12-\(endDay)"
+        
+        task.updateDates(startDate: startDate, endDate: endDate)
+        XCTAssert(task.startDate == startDateString)
+        XCTAssert(task.endDate == endDateString)
+        
+    }
+    
+    /**
+     Test that deleteSelf deletes the Task, its Tags, its TaskInstances, and its TaskTargetSets
+     */
+    func testDeleteTask() throws {
+        let startDate = Calendar.current.date(from: DateComponents(year: 2019, month: 1, day: 1))!
+        let endDate = Calendar.current.date(from: DateComponents(year: 2019, month: 3, day: 1))!
+        let tts = TaskTargetSet(entity: TaskTargetSet.getEntityDescription(CDCoordinator.moc)!,
+                                insertInto: CDCoordinator.moc, min: 0, max: 3, minOperator: 1, maxOperator: 1, priority: 0,
+                                pattern: DayPattern(dow: Set([1,2,3,4,5,6,7]), wom: Set([]), dom: Set([])))
+        
+        let task = Task(entity: Task.getEntityDescription(CDCoordinator.moc)!,
+                        insertInto: CDCoordinator.moc,
+                        name: "Task", tags: ["Tag1","Tag2"],
+                        startDate: startDate, endDate: endDate,
+                        targetSets: [tts])
+        
+        task.deleteSelf()
+        let tasks = try CDCoordinator.moc.fetch(Task.fetchRequest()) as [Task]
+        let tags = try CDCoordinator.moc.fetch(Tag.fetchRequest()) as [Tag]
+        let targetSets = try CDCoordinator.moc.fetch(TaskTargetSet.fetchRequest()) as [TaskTargetSet]
+        let instances = try CDCoordinator.moc.fetch(TaskInstance.fetchRequest()) as [TaskInstance]
+        XCTAssert(tasks.count == 0)
+        XCTAssert(tags.count == 0)
+        XCTAssert(targetSets.count == 0)
+        XCTAssert(instances.count == 0)
+    }
+    
+}
+
+// MARK: - Performance tests
+
+extension TaskTests {
+    
+    /**
      Test the time it takes updateTags to add 1,000 new Tags to the MOC
      */
     func testPerformanceAddNewTags() throws {
@@ -107,32 +164,6 @@ class TaskTests: XCTestCase {
         self.measure {
             task.updateTags(newTagNames: [])
         }
-        
-    }
-    
-    // MARK: - updateTags tests
-    
-    /**
-     Test that updateDates updates the Task's dates and saves in the expected String format
-     */
-    func testUpdateDates() {
-        
-        let task = Task(context: CDCoordinator.moc)
-        XCTAssert(task.startDate == nil)
-        XCTAssert(task.endDate == nil)
-        
-        // Start date = GMT April 17, 1998 13:49:35
-        // End date = GMT December 5, 1998 01:22:55
-        let startDate = Date(timeIntervalSince1970: 892820975)
-        let endDate = Date(timeIntervalSince1970: 912820975)
-        let startDay = String(format: "%02d", Calendar.current.component(.day, from: startDate))
-        let endDay = String(format: "%02d", Calendar.current.component(.day, from: endDate))
-        let startDateString = "1998-04-\(startDay)"
-        let endDateString = "1998-12-\(endDay)"
-        
-        task.updateDates(startDate: startDate, endDate: endDate)
-        XCTAssert(task.startDate == startDateString)
-        XCTAssert(task.endDate == endDateString)
         
     }
     
