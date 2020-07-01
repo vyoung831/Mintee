304ebfb999e6ad6fd8dda8f8164332ff246bccde
Vincent Young <v.young831@yahoo.com>
2020-06-30 20:59:43 -0400

    Cleaned up existing UI tests

--- MuUITests/MainTabBarUITests.swift
+++ MuUITests/MainTabBarUITests.swift
@@ -2,7 +2,7 @@
 //  MainTabBarUITests.swift
 //  MuUITests
 //
-//  This XCTestCase tests the navigation between Mu's main tabs
+//  These UI tests test the navigation between Mu's main tabs
 //
 //  Created by Vincent Young on 4/4/20.
 //  Copyright © 2020 Vincent Young. All rights reserved.
@@ -11,32 +11,14 @@
 import XCTest
 
 class MainTabBarUITests: XCTestCase {
-
-    let analysisTabName: String = "Analysis"
-    let analysisNavBarName: String = "Analysis"
-    let manageTabName :String = "Manage"
-    let manageNavBarName :String = "Manage"
-    let todayTabName :String = "Today"
-    let todayNavBarName :String = "Today"
-    let settingsTabName :String = "Settings"
-    let settingsNavBarName :String = "Settings"
     
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         XCUIDevice.shared.orientation = .portrait
     }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    
+    override func tearDown() { }
     
     func testNavigateToAnalysisTab() {
         let app = XCUIApplication()
@@ -61,5 +43,5 @@ class MainTabBarUITests: XCTestCase {
         app.tabBars.buttons["Settings"].tap()
         XCTAssert(app.navigationBars["Settings"].exists)
     }
-
+    
 }
--- MuUITests/MuUITests.swift
+++ MuUITests/MuUITests.swift
@@ -2,7 +2,7 @@
 //  MuUITests.swift
 //  MuUITests
 //
-//  Created by Vincent Young on 3/31/20.
+//  Created by Vincent Young on 6/30/20.
 //  Copyright © 2020 Vincent Young. All rights reserved.
 //
 
@@ -10,27 +10,19 @@ import XCTest
 
 class MuUITests: XCTestCase {
 
-    override func setUp() {
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
 
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
 
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    override func tearDownWithError() throws {}
 
     func testLaunchPerformance() {
         if #available(iOS 13.0, *) {
@@ -40,4 +32,5 @@ class MuUITests: XCTestCase {
             }
         }
     }
+
 }
