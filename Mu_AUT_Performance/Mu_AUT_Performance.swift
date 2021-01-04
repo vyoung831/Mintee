44e1e4a67a6db5ab712067b1227bb9b5e7a9d995
Vincent Young <v.young831@yahoo.com>
2021-01-03 20:56:07 -0500

    Create new Mu_AUT_Performance target

---
+++ Mu_AUT_Performance/Mu_AUT_Performance.swift
@@ -0,0 +1,21 @@
+//
+//  Mu_AUT_Performance.swift
+//  Mu_AUT_Performance
+//
+//  Created by Vincent Young on 1/3/21.
+//  Copyright © 2021 Vincent Young. All rights reserved.
+//
+
+import XCTest
+
+class Mu_AUT_Performance: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
