0a4eef0822ea1b6af2b6526d648c2ebf1d4aaae5
Vincent Young <v.young831@yahoo.com>
2021-03-07 20:36:17 -0500

    Repair check-git-changes.sh and assign unique exit codes

--- Versions/check-git-changes.sh
+++ Versions/check-git-changes.sh
@@ -5,26 +5,28 @@
 # This script checks that the current local branch is tracking from and up-to-date with origin/master.
 #
 
+# Get the (upstream:short) format of HEAD
 UPSTREAM_NAME=$(git for-each-ref --format="%(upstream:short)" $(git symbolic-ref HEAD))
 
+echo $UPSTREAM_NAME
 # Check that repo is tracking with origin/master
 if [[ $UPSTREAM_NAME != "origin/master" ]]; then
     exit 1
 fi
 
-# Fetch and check that local HEAD is in sync with remote master branch
 git fetch
 
-if [[ $(git rev-list -n 1 HEAD) != $(git rev-list -n 1 --remotes=$UPSTREAM_NAME) ]]; then
-    exit 1
+# Fetch and check that local HEAD is in sync with remote master branch
+if [[ $(git rev-list -n 1 HEAD) != $(git rev-list -n 1 $UPSTREAM_NAME) ]]; then
+    exit 2
 fi
 
 # Check that merge conflicts don't exist
-if [[ $(file -e .git/MERGE_HEAD) ]]: then
-    exit 1
+if [[ $(test -e ../.git/MERGE_HEAD) ]]; then
+    exit 3
 fi
 
 # Check that no staged or unstaged changes remain
 if [[ $(git --no-pager diff) ]] || [[ $(git --no-pager diff --cached) ]] ; then
-    exit 1
+    exit 4
 fi
